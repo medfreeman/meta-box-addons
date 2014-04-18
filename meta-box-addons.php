7a88a536648521fa9182b6e3882dc91552e78559
Mehdi Lahlou <mehdi.lahlou@free.fr>
2014-04-18 15:46:55 +0200

    fixed update url

--- meta-box-addons.php
+++ meta-box-addons.php
@@ -21,7 +21,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/medfreeman/meta-box-addons.git
+ * GitHub Plugin URI: https://github.com/medfreeman/meta-box-addons
  */
 
 // If this file is called directly, abort.
